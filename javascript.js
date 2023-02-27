06904ec716e213cbf23c1d102fdb30fca9db1587
crazy_squirrel <100859780+13011brett@users.noreply.github.com>
2023-02-26 22:23:51 -0500

    Basic operations & requirements met

--- javascript.js
+++ javascript.js
@@ -1,20 +1,38 @@
 var numbers = document.querySelectorAll(".num");
 var numDisplay = document.querySelector('.input-display');
 var clearNum = document.querySelector('._numac');
-var symbols = document.querySelectorAll('.symbols');
+var symbols = document.querySelectorAll('.symbol');
+
+var currentSymbol;
 
 var currentNumber = '0';
-var functionNumber;
+var functionNumber = '';
 
 numbers.forEach(number => number.addEventListener('click', () => {
     if(currentNumber === '0') currentNumber = ''; 
-    currentNumber += number.textContent;
-    numDisplay.textContent = currentNumber;
+    currentSymbol != undefined 
+    ? functionNumber += number.textContent 
+    : currentNumber += number.textContent;
+
+    currentSymbol != undefined 
+     ? numDisplay.textContent = currentNumber + " " + currentSymbol + " " + functionNumber
+     : numDisplay.textContent = currentNumber;
+    
     
 }))
 
-symbols.forEach(symbol => symbol.addEventListener('click', () => 
-                currentNumber = operate(currentNumber, functionNumber, symbol.textContent)))
+symbols.forEach(symbol => symbol.addEventListener('click', () => {
+    if(symbol.textContent == '='){
+        currentNumber = operate(currentNumber, functionNumber, currentSymbol);
+        numDisplay.textContent += " = " + currentNumber;
+        functionNumber = '';
+        currentSymbol = undefined;
+        return;
+    }
+    currentSymbol = symbol.textContent;
+    
+    numDisplay.textContent = currentNumber + " " + currentSymbol + " ";
+}))
 
 function equals(number){
     return Number(number);
@@ -24,12 +42,17 @@ clearNum.addEventListener('click', () => clear());
 
 function clear(){
     currentNumber = '0';
+    functionNumber = '';
+    currentSymbol = undefined;
     numDisplay.textContent = currentNumber;
 }
 
 
+
+
+
 function operate(num1, num2, symbol){
-    if(num2 == undefined) return;
+    if(num2 == undefined || num1 == undefined) return 0;
     switch (symbol){
         case "X":
             return mult(num1, num2);
@@ -44,7 +67,8 @@ function operate(num1, num2, symbol){
             return sub(num1, num2);
 
         default:
-            return "ERROR";
+            clear();
+            return 0;
 
     }
 }
@@ -65,3 +89,5 @@ function mult(num1, num2){
 function div(num1, num2){
     return Math.round((Number(num1) / Number(num2)) * 100)/100;
 }
+
+ 
